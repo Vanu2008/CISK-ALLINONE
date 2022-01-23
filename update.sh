922405d3873da36f03e9ac80527719c8aefd11e0
VANU <96168458+Vanu2008@users.noreply.github.com>
2022-01-23 14:57:07 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,56 @@
+echo "
+echo " \o              o/         o/        \o          o/     o__ __o      o         o
+echo "< >            < >         / \        / \\o      / \   />       \\   /|         |\
+echo " \o            o/       \o/   \o      \o/  \     \o/   \o____         |         |
+echo "   \          /          |__ __|>      |   <\     |        \_\__o__   o__/_ _\__o
+echo "   <\        />         /       \     / \    \o  / \             \\   |         |
+echo "    \\o    o//        o/         \\o  \o/      \ \o/   \         /   <o>       <o>
+echo "       \  /          /\            \   |       <\ |     o       o     |         |
+echo "       <\/>         />             <\ /|         < \    <\__ __/>    / \       / \
+echo "                                               ";
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackingtool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/hackingtool
+
+clear
+
+mkdir hackingtool
+
+clear
+
+cd hackingtool
+
+clear
+
+git clone
+
+clear
+
+cd hackingtool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
